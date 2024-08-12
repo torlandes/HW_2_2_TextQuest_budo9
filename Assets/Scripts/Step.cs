acfcd0b00df57090b3c99792e77fa2abe85786b8
torlandes <torlandesx@gmial.com>
2024-08-13 02:08:17 +0300

    Added scripts and game objects for text quest

---
+++ Assets/Scripts/Step.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Step : MonoBehaviour
+{
+    #region Variables
+
+    [TextArea(5, 10)]
+    [SerializeField] private string _answers;
+    [TextArea(3, 10)]
+    [SerializeField] private string _description;
+
+    [SerializeField] private Step[] _nextStep;
+    
+    [SerializeField] private string _locationName;
+    
+    [SerializeField] private Sprite _currentLocationImage;
+    
+    public bool isWin;
+
+
+    #endregion
+
+    #region Properties
+
+    public string Answers => _answers;
+    public string Description => _description;
+    
+    public string Location => _locationName;
+
+    public Step[] NextSteps => _nextStep;
+    
+    public Sprite CurrentLocationImage => _currentLocationImage;
+    
+    #endregion
+}
---
+++ Assets/Scripts/TextQuest.cs
@@ -0,0 +1,76 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TextQuest : MonoBehaviour
+{
+    #region Variables
+
+    [SerializeField] private TMP_Text _descriptionLabel;
+    [SerializeField] private TMP_Text _answerLabel;
+    [SerializeField] private TMP_Text _locationLabel;
+
+    [SerializeField] private Step _startStep;
+    [SerializeField] private Step _currentStep;
+    
+    [SerializeField] private Image _currentLocationImage;
+
+    #endregion
+
+    #region Unity lifecycle
+
+    private void Start()
+    {
+        SetCurrentStepAndUpdateUi(_startStep);
+    }
+
+    private void Update()
+    {
+        for (int i = 1; i < 9; i++)
+        {
+            if (Input.GetKeyDown(i.ToString()))
+            {
+                TryGoToNextStep(i);
+            }
+        }
+        if (_currentStep.isWin && Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    #endregion
+
+    #region Private methods
+
+    private void SetCurrentStepAndUpdateUi(Step step)
+    {
+        _currentStep = step;
+
+        UpdateUi();
+    }
+
+    private void TryGoToNextStep(int number)
+    {
+        int nextStepsCount = _currentStep.NextSteps.Length;
+        if (number > nextStepsCount)
+        {
+            return;
+        }
+
+        int nextStepIndex = number - 1;
+        Step nextStep = _currentStep.NextSteps[nextStepIndex];
+        SetCurrentStepAndUpdateUi(nextStep);
+    }
+
+    private void UpdateUi()
+    {
+        _descriptionLabel.text = _currentStep.Description;
+        _answerLabel.text = _currentStep.Answers;
+        _locationLabel.text = _currentStep.Location;
+        _currentLocationImage.sprite = _currentStep.CurrentLocationImage;
+    }
+
+    #endregion
+}
